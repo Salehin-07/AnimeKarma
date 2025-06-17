9958f90dfcf5fbb314d9738504b0b3277e8ab842
Salehin-07 <mdabusalehin123@gmail.com>
2025-06-17 15:41:16 +0600

    Update settings.py

--- AnimeNews/settings.py
+++ AnimeNews/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = get_random_secret_key()
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'carmaanime.onrender.com',
+    'animecarma.onrender.com',
     '100.20.92.101',
     '44.225.181.72', 
     '44.227.217.144',
