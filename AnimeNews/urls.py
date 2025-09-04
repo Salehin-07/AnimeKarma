fe3b8797e201928822f5ef9b3d25c71b09a13b56
Salehin-07 <mdabusalehin123@gmail.com>
2025-09-04 23:21:45 +0600

    Update urls.py

--- AnimeNews/urls.py
+++ AnimeNews/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from aninews import views  # Use lowercase app name
 
 urlpatterns = [
     path("", views.home, name='home'),
-    path('admin/', admin.site.urls),
 ]
